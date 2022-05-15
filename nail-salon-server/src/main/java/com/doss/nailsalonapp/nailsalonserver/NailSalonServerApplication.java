df72e48eb128f27860b4a83e34de4e813bae9740
Chandy Doss <chandy.doss@gmail.com>
2022-05-15 09:25:20 -0400

    adding project template

---
+++ nail-salon-server/src/main/java/com/doss/nailsalonapp/nailsalonserver/NailSalonServerApplication.java
@@ -0,0 +1,13 @@
+package com.doss.nailsalonapp.nailsalonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NailSalonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NailSalonServerApplication.class, args);
+	}
+
+}
---
+++ nail-salon-server/src/test/java/com/doss/nailsalonapp/nailsalonserver/NailSalonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doss.nailsalonapp.nailsalonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NailSalonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
