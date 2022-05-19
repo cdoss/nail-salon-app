33a52de511a48f553fb4209415c29f8a1c2a95c6
Chandy Doss <chandy.doss@gmail.com>
2022-05-18 22:12:36 -0400

    renaming package

--- nail-salon-server/src/main/java/com/cdoss/nailsalon/NailSalonServerApplication.java
+++ nail-salon-server/src/main/java/com/cdoss/nailsalon/NailSalonServerApplication.java
@@ -1,4 +1,4 @@
-package com.doss.nailsalonapp.nailsalonserver;
+package com.cdoss.nailsalon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
